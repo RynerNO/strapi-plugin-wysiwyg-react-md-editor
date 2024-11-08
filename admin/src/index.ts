8d00da6eff7005a19c090f7b14e3f56301e3e46a
RynerNO <ryner.no@gmail.com>
2024-11-08 08:44:10 +0100

    remove unused code for translations

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -2,7 +2,6 @@ import pluginPkg from "../../package.json";
 import pluginId from "./pluginId";
 import { Initializer } from "./components/Initializer";
 import { Editor as ReactMdEditor } from "./components/ReactMdEditor";
-import { getTranslation } from "./utils/getTranslation";
 
 const name = pluginPkg.strapi.name;
 
@@ -17,26 +16,5 @@ export default {
     };
 
     app.registerPlugin(plugin);
-  },
-  async registerTrads({ locales }: any) {
-    const importedTrads = await Promise.all(
-      locales.map((locale: any) => {
-        return import(`./translations/${locale}.json`)
-          .then(({ default: data }) => {
-            return {
-              data: data,
-              locale,
-            };
-          })
-          .catch(() => {
-            return {
-              data: {},
-              locale,
-            };
-          });
-      })
-    );
-
-    return Promise.resolve(importedTrads);
   },
 };
